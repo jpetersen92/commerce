746febdc71a7405d19dd3488e376e477f84a87ab
Joshua Petersen <joshpetersen@Joshuas-MacBook-Air.local>
2021-11-19 14:54:12 -0800

    added routing

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Typography, Button, Grid } from '@material-ui/core';
+import CartItem from "./CartItem/CartItem";
 
 import useStyles from './styles'
 
@@ -17,8 +18,7 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        {/* <CartItem /> */}
-                        <div>{item.name}</div>
+                        <CartItem item={item}/>
                     </Grid>
                 ))}
             </Grid>
@@ -37,7 +37,7 @@ const Cart = ({ cart }) => {
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
+            <Typography className={classes.title} variant='h3'gutterBottom >Your Shopping Cart</Typography>
             { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
         </Container>
     )
